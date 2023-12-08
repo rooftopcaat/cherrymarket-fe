a0d9c792db3359baee1db6bd35085780d41355a6
[kim1334] <[mo131@naver.com]>
2023-12-08 15:36:51 +0900

    Rename : 1차 pull

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import './App.css';
 import Router from './Router/router.jsx';
 
-function App() {
-  return (
-    <div className="App">
-import Router from './Router';
 
 function App() {
   return (
